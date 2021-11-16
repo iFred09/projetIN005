ad4fcd48686d2b0ee028f9095b8ff9271a33b8a7
iFred09 <frederic142@outlook.com>
2021-11-16 12:31:37 +0100

    fin 2.2

--- Fichiers Python Fournis-20211116/q2.1-2.2.py
+++ Fichiers Python Fournis-20211116/q2.1-2.2.py
@@ -29,15 +29,35 @@ t6 = Transition(s2,"b",s1)
 
 auto = Automate([t1,t2,t3,t4,t5,t6])
 print(auto)
-auto.show("A_ListeTrans")
+# auto.show("A_ListeTrans")
 
 auto1 = Automate([t1,t2,t3,t4,t5,t6], [s0,s1,s2])
 print(auto1)
-auto.show("2.1.2")
+# auto.show("2.1.2")
 
 auto2=Automate.creationAutomate("auto.txt")
 print(auto2)
-auto.show("2.1.3")
+# auto.show("2.1.3")
 
 # Ex 2.2
 
+t = Transition(0,"a",1)
+auto.removeTransition(t1)
+print(auto)
+# auto.show("2.2.1-withoutt1")
+auto.addTransition(t1)
+print(auto)
+
+auto.removeState(s1)
+print(auto)
+# auto.show("2.2.2-withouts1")
+auto.addState(s1)
+print(auto)
+# auto.show("2.2.2-withs1")
+
+s2=State(0,True,False)
+auto.addState(s2)
+print(auto)
+# auto.show("2.2.2-withs2")
+
+print(auto1.getListTransitionsFrom(s1))
